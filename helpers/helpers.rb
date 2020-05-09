6f8e22687d903a7cd3437b64b5a3f5e02c79eb12
Vitalii Kuzminov <vkuzminov@artec-group.com>
2020-05-09 19:42:46 +0300

    change helpers

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -1,28 +1,4 @@
 def request_into_database(params)
-  # date = %|'#{Time.now.strftime('%d-%m-%Y')}'|
-  # vacancy_name = params['name'].nil? ? %|'-'| : %|'#{params['name']}'|
-  # city = params['area'].nil? ? %|'-'| : %|'#{params['area']['name']}'|
-  # salary_from =
-  #   if params['salary'].nil?
-  #     %|'-'|
-  #   elsif params['salary'] && params['salary']['from'].nil?
-  #     %|'-'|
-  #   else
-  #     %|'#{params['salary']['from']}'|
-  #   end
-  #
-  # salary_to =
-  #   if params['salary'].nil?
-  #     %|'-'|
-  #   elsif params['salary'] && params['salary']['to'].nil?
-  #     %|'-'|
-  #   else
-  #     %|'#{params['salary']['to']}'|
-  #   end
-  #
-  # currency = params['salary'].nil? ? %|'-'| : %|'#{params['salary']['currency']}'|
-  # company = params['employer'].nil? ? %|'-'| : %|'#{params['employer']['name']}'|
-
   execute = proc do |params, par1, par2|
     if params[par1].nil?
       %|'-'|
@@ -44,14 +20,3 @@ def request_into_database(params)
   'INSERT INTO vacancies (date, vacancy_name, city, salary_from, salary_to, currency, company) '\
   "VALUES(#{date}, #{vacancy_name}, #{city}, #{salary_from}, #{salary_to}, #{currency}, #{company})"
 end
-
-# create table vacancies (
-# id serial primary key,
-# date varchar NOT NULL,
-# vacancy_name varchar (255),
-# city varchar (255) NOT NULL,
-# salary_from varchar (255) NOT NULL,
-# salary_to varchar (255) NOT NULL,
-# currency varchar (10),
-# company varchar (255)
-# )
